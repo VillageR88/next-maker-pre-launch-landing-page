94e29aab9ce734df1b45464a81c9292679b4657d
VillageR88 <villager88pl@gmail.com>
2024-05-26 05:57:16 +0200

    MainRow2 layout improved and Image added for rows
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/components/MainRow2.tsx
+++ app/components/MainRow2.tsx
@@ -1,38 +1,52 @@
+import Image from 'next/image';
+import img1 from '@/public/assets/images/illustration-passions.svg';
+import img2 from '@/public/assets/images/illustration-financial-freedom.svg';
+import img3 from '@/public/assets/images/illustration-lifestyle.svg';
+import img4 from '@/public/assets/images/illustration-work-anywhere.svg';
+
 export default function MainRow2() {
   const listItems = [
     {
       title: 'Indulge your passions',
       description: "Your passions shouldn't be just for the weekend. Earn a living doing what you love.",
+      src: img1 as string,
     },
     {
       title: 'Gain financial freedom',
       description: 'Start making money work for you. There’s nothing quite like earning while you sleep.',
+      src: img2 as string,
     },
     {
       title: 'Choose your lifestyle',
       description: 'Own your daily schedule. Fancy a lie-in? Go for it! Take charge of your week.',
+      src: img3 as string,
     },
     {
       title: 'Work from anywhere',
       description: 'Selling online means not being pinned down. Want to work AND travel? Go for it!',
+      src: img4 as string,
     },
   ];
 
   const MiddleBlock = ({
     title,
     description,
+    src,
     classExtension,
   }: {
     title: string;
     description: string;
+    src: string;
     classExtension: string;
   }) => {
     return (
       <li className={`${classExtension} flex h-[335px] w-[255px] flex-col gap-[48px]`}>
-        <div className="h-[164px] w-[160px] rounded-[45px] bg-[#093F68]"></div>
+        <div className="flex h-[164px] w-[160px] items-center justify-center rounded-[45px] bg-[#093F68]">
+          <Image src={src} alt={title} />
+        </div>
         <section className="flex flex-col gap-[23px]">
           <h3>{title}</h3>
-          <p>{description}</p>
+          <p className="text-[#777F98]">{description}</p>
         </section>
       </li>
     );
@@ -45,6 +59,7 @@ export default function MainRow2() {
           classExtension={index % 2 ? 'self-end' : 'sel-start'}
           key={index}
           title={item.title}
+          src={item.src}
           description={item.description}
         />
       ))}
